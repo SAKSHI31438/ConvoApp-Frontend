59099125355df35c2dd4d92a7d5f75bab4e1c326
SAKSHI31438 <kadamsakshi976@gmail.com>
2026-01-06 23:54:09 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <>
+      <div className="item-center justify-center">
+        <hr className=" border-black" />
+        <h1 className="text-center py-3 text-sm">
+          Copyright © 2026 - All right reserved
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
